a0fde7d80b2595d68f3d0f4628d0b563d179f9bc
Karmit Personal <karmit199@gmail.com>
2021-04-05 13:35:00 -0400

    Initial Commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReviewListComponent } from './review-list/review-list.component';
+import { ReviewComponent } from './review-list/review/review.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { BookCardComponent } from './book-card/book-card.component';
+import { OrderTotalComponent } from './order-total/order-total.component';
+import { OrderTotalDetailComponent } from './order-total-detail/order-total-detail.component';
+import { AddInfoComponent } from './add-info/add-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewListComponent,
+    ReviewComponent,
+    NavbarComponent,
+    CategoryComponent,
+    CatalogueComponent,
+    BookCardComponent,
+    OrderTotalComponent,
+    OrderTotalDetailComponent,
+    AddInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/order-total-detail/order-total-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-total-detail',
+  templateUrl: './order-total-detail.component.html',
+  styleUrls: ['./order-total-detail.component.scss']
+})
+export class OrderTotalDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
